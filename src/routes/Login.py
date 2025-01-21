18ccc79181a9afb9b3983e76c77afa0028ebc562
Menciajp <menciajp.27@gmail.com>
2025-01-21 17:52:22 -0300

    Pasar login a un metodo post

--- src/routes/Login.py
+++ src/routes/Login.py
@@ -1,5 +1,5 @@
 #para poder hacer "sistema de rutas"
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 #modelos
 from models.LoginModel import LoginModel
 #utils
@@ -7,13 +7,29 @@ from utils.Security import Security
 main = Blueprint('login', __name__)
 
 #GET
-@main.route('/<usuario>/<contrasena>', methods=['GET'])
-def login(usuario, contrasena):
+@main.route('/', methods=['POST'])
+def login():
     try:
-        persona = LoginModel.login(usuario,contrasena)
+        required_fields = ['usuario','contrasenia']
+            
+        # Obtener datos de la solicitud
+        data = request.json
+            
+        # Validar que existan y no estén vacíos
+        empty_fields = [
+            field for field in required_fields
+            if not data.get(field) or
+            (isinstance(data.get(field), str) and data.get(field).strip() == "") or
+            (isinstance(data.get(field), list) and len(data.get(field)) == 0)
+        ]
+        if empty_fields:
+            return jsonify({'error': f"Los siguientes campos están vacíos: {', '.join(empty_fields)}"}), 400
+        usuario = data['usuario']
+        contrasenia = data['contrasenia']
+        persona = LoginModel.login(usuario,contrasenia)
         if persona != None:
             encoded_token = Security.generate_token(persona)
-            return jsonify({'success':True, 'token': encoded_token}), 200
+            return jsonify({'success':True, 'token': encoded_token, 'rol' : persona.rol, 'usuario': persona.usuario}, ), 200
         else:
             return jsonify({'message': 'Usuario o contraseña incorrectos'}), 404
     except Exception as e:
